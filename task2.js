4e11abe69ad329db20f782e5024cb39977460630
Rumyana Atanasova <rumyana_atanasova@epam.com>
2022-05-13 12:06:56 +0300

    reverted to streams

--- task2.js
+++ task2.js
@@ -1,31 +1,28 @@
-const fs = require('fs');
-const fsPromises = fs.promises;
-const csv=require('csvtojson');
-const csvFilePath='./csv/nodejs-hw1-ex1.csv';
-
-async function readFromFile(){
-    const jsonArray=await csv({
-        noheader: false,
-        headers: ['book','author', 'amount', 'price'],
-    }).fromFile(csvFilePath);
-    write(jsonArray);
+const fs = require("fs");
+const csv = require("csvtojson");
+const csvFilePath = "./csv/nodejs-hw1-ex1.csv";
+const writerStream = fs.createWriteStream("./nodejs19-hw1-ex2.txt");
+async function readFromFile() {
+  const jsonArray = await csv({
+    noheader: false,
+    headers: ["book", "author", "amount", "price"],
+  }).fromFile(csvFilePath);
+  write(jsonArray);
 }
 
-async function write(data){
-    try{
-        for(let i=0; i<data.length;i++){
-            if(i===0){
-                await fsPromises.writeFile('./nodejs19-hw1-ex2.txt', JSON.stringify(data[i]));
-                await fsPromises.appendFile('./nodejs19-hw1-ex2.txt', '\n')
-                continue;
-            }
-            await fsPromises.appendFile('./nodejs19-hw1-ex2.txt', JSON.stringify(data[i]));
-            await fsPromises.appendFile('./nodejs19-hw1-ex2.txt', '\n')
-        }
-           
-    }catch(e){
-        console.error(e.message)
+function write(data) {
+  data.forEach((chunk, index) => {
+    try {
+      if (index === data.length - 1) {
+        writerStream.write(JSON.stringify(chunk), "UTF8");
+        writerStream.end();
+        return;
+      }
+      writerStream.write(JSON.stringify(chunk), "UTF8");
+      writerStream.write("\n", "UTF8");
+    } catch (e) {
+      console.error(e.message);
     }
-    
+  });
 }
 readFromFile();
