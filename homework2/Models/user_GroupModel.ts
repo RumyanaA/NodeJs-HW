b603b3b405ef6465a80ac8d03d9b6fd3f9e438f7
Rumyana Atanasova <rumyana_atanasova@epam.com>
2022-07-18 09:50:55 +0300

    removed commented code

--- homework2/Models/user_GroupModel.ts
+++ homework2/Models/user_GroupModel.ts
@@ -31,29 +31,3 @@ User_Group.init(
     },
     { sequelize, timestamps: false }
 );
-
-// import User from './userModel.js';
-// import Group from './groupModel.js';
-
-// const User_Group = sequelize.define(
-//     'User_Group',
-//     {
-//         UserId: {
-//             type: DataTypes.INTEGER,
-//             references: {
-//                 model: User,
-//                 key: 'id'
-//             }
-//         },
-//         GroupId: {
-//             type: DataTypes.INTEGER,
-//             references: {
-//                 model: Group,
-//                 key: 'id'
-//             }
-//         }
-//     },
-//     { timestamps: false }
-// );
-
-// export default User_Group;
