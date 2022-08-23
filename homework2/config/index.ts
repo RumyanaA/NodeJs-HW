fdc4feaa01a032b386d72f4b854662e9dd62d89b
Rumyana Atanasova <rumyana_atanasova@epam.com>
2022-08-23 11:19:50 +0300

    tests init

--- homework2/config/index.ts
+++ homework2/config/index.ts
@@ -13,3 +13,10 @@ export default {
     password: process.env.DB_PASSWORD,
     jwtSecret: process.env.JWT_SECRET
 };
+
+// export default {
+//     username:'postgres',
+//     password:'1234',
+//     jwtSecret:'1D6DA62DBDA473A8E8F462D3A5A78E270C244F040E9035D834B38F5ABF4E7B96'
+// };
+
